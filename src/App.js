fb11fcef8738a792158e82715d82e568490f0735
Adonis Martin quiñones <adonisdesuvikdo@gmail.com>
2019-12-16 19:42:36 -0800

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="my-header py-3">
+        <div className="row flex-nowrap justify-content-between align-items-center">
+          <div className="col-4"></div>
+          <div className="col-4 text-center">
+            <h1>Store Visits</h1>
+          </div>
+          <div className="col-4"></div>
+        </div>
       </header>
     </div>
   );
