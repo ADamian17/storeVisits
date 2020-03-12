8ad63c8a1410ae8ed401dc365aa8246afbd25666
Adonis Martin quiñones <adonisdesuvikdo@gmail.com>
2020-03-12 09:37:17 -0700

    Updates

---
+++ precache-manifest.bff532168218c0af72604be5fed160bb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "17f50ea4e1d6fe53ace5bd6c3384afe0",
+    "url": "/storevisits/index.html"
+  },
+  {
+    "revision": "2fdd6923083c1f91358b",
+    "url": "/storevisits/static/css/main.2669fb86.chunk.css"
+  },
+  {
+    "revision": "99dbb7e833949ff04c79",
+    "url": "/storevisits/static/js/2.bba8f065.chunk.js"
+  },
+  {
+    "revision": "c6d01cb19213691bc81a274434d00fe7",
+    "url": "/storevisits/static/js/2.bba8f065.chunk.js.LICENSE"
+  },
+  {
+    "revision": "2fdd6923083c1f91358b",
+    "url": "/storevisits/static/js/main.277ad3d9.chunk.js"
+  },
+  {
+    "revision": "dfa6b11f9a356a145c40",
+    "url": "/storevisits/static/js/runtime-main.2fb70969.js"
+  }
+]);
